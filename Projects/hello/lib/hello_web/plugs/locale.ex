39b272d8b6762f598e44e720010d94c582fea06a
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-17 15:30:30 -0300

    Fix two little mistakes(do: and plug:) and show the locale in main page

--- Projects/hello/lib/hello_web/plugs/locale.ex
+++ Projects/hello/lib/hello_web/plugs/locale.ex
@@ -3,7 +3,7 @@ defmodule HelloWeb.Plugs.Locale do
 
   @locales ["en", "fr", "pt"]
 
-  def init(default), do :default
+  def init(default), do: default
 
   def call(%Plug.Conn{params: %{"locale" => loc}} = conn, _default) when loc in @locales do
     assign(conn, :locale, loc)
