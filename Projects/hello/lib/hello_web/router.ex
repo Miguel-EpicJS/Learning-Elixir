5c19f038d82225f3a10d502c0c71b6f5b083874d
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-17 14:23:53 -0300

    Hello messager route(reusing Hello Controller)

--- Projects/hello/lib/hello_web/router.ex
+++ Projects/hello/lib/hello_web/router.ex
@@ -18,6 +18,7 @@ defmodule HelloWeb.Router do
 
     get "/", PageController, :index
     get "/hello", HelloController, :index
+    get "/hello/:messager", HelloController, :index
   end
 
   # Other scopes may use custom stacks.
