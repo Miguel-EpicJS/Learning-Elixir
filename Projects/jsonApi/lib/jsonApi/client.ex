8bc21e91083725affa30a2981cd9f3448302c86c
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-18 11:04:18 -0300

    Some validates

--- Projects/jsonApi/lib/jsonApi/client.ex
+++ Projects/jsonApi/lib/jsonApi/client.ex
@@ -1,6 +1,7 @@
 defmodule JsonApi.Client do
   use Ecto.Schema
   import Ecto.Changeset
+  alias JsonApi.Client
 
   schema "clients" do
     field :address, :string
@@ -15,6 +16,10 @@ defmodule JsonApi.Client do
   def changeset(client, attrs) do
     client
     |> cast(attrs, [:name, :price, :address, :desc])
-    |> validate_required([:name, :price, :address, :desc])
+    |> validate_required([:name, :price, :address])
+    |> validate_length(:name, min: 3)
+    |> validate_length(:name, max: 250)
+    |> validate_length(:address, max: 250)
+    |> validate_length(:desc, max: 250)
   end
 end
