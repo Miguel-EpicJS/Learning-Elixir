79257989d6b06696a06440d296e7da7ddf54fdd4
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-18 10:45:52 -0300

    Simple Client router, map to /api/clients

--- Projects/jsonApi/lib/jsonApi_web/router.ex
+++ Projects/jsonApi/lib/jsonApi_web/router.ex
@@ -17,6 +17,7 @@ defmodule JsonApiWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/api/clients", ClientController, :index
   end
 
   # Other scopes may use custom stacks.
