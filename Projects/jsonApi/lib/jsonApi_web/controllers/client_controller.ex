5f1e6f97bf096eb6e5e74669d2955b7ef4af019e
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-18 14:47:39 -0300

    All controllers for a restful service(simple render)

--- Projects/jsonApi/lib/jsonApi_web/controllers/client_controller.ex
+++ Projects/jsonApi/lib/jsonApi_web/controllers/client_controller.ex
@@ -4,4 +4,22 @@ defmodule JsonApiWeb.ClientController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+  def edit(conn, _params) do
+    render(conn, "edit.html")
+  end
+  def new(conn, _params) do
+    render(conn, "new.html")
+  end
+  def show(conn, _params) do
+    render(conn, "show.html")
+  end
+  def create(conn, _params) do
+    render(conn, "create.html")
+  end
+  def update(conn, _params) do
+    render(conn, "update.html")
+  end
+  def delete(conn, _params) do
+    render(conn, "delete.html")
+  end
 end
