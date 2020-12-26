15534620d08ab062b24ac646bb36ce4f84591c05
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-26 10:28:39 -0300

    RESTful routes for /user

--- Projects/user/lib/user_web/router.ex
+++ Projects/user/lib/user_web/router.ex
@@ -17,6 +17,7 @@ defmodule UserWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/user", UserController
   end
 
   # Other scopes may use custom stacks.
