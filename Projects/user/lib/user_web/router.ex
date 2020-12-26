54d1bab6007d6645d7ad00f1bed3624d293a73a1
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-26 11:22:20 -0300

    Simple route change

--- Projects/user/lib/user_web/router.ex
+++ Projects/user/lib/user_web/router.ex
@@ -17,7 +17,7 @@ defmodule UserWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
-    resources "/user", UserController
+    resources "/users", UserController
   end
 
   # Other scopes may use custom stacks.
