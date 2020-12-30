26b31d294934a520d178fab2850614780d3a36b1
CodigoDeSenior <migueljnm840@gmail.com>
2020-12-30 09:48:05 -0300

    Controllers for user

---
+++ Projects/client/lib/client_web/controllers/user_controller.ex
@@ -0,0 +1,62 @@
+defmodule ClientWeb.UserController do
+  use ClientWeb, :controller
+
+  alias Client.Clients
+  alias Client.Clients.User
+
+  def index(conn, _params) do
+    users = Clients.list_users()
+    render(conn, "index.html", users: users)
+  end
+
+  def new(conn, _params) do
+    changeset = Clients.change_user(%User{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    case Clients.create_user(user_params) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "User created successfully.")
+        |> redirect(to: Routes.user_path(conn, :show, user))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    user = Clients.get_user!(id)
+    render(conn, "show.html", user: user)
+  end
+
+  def edit(conn, %{"id" => id}) do
+    user = Clients.get_user!(id)
+    changeset = Clients.change_user(user)
+    render(conn, "edit.html", user: user, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "user" => user_params}) do
+    user = Clients.get_user!(id)
+
+    case Clients.update_user(user, user_params) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "User updated successfully.")
+        |> redirect(to: Routes.user_path(conn, :show, user))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "edit.html", user: user, changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    user = Clients.get_user!(id)
+    {:ok, _user} = Clients.delete_user(user)
+
+    conn
+    |> put_flash(:info, "User deleted successfully.")
+    |> redirect(to: Routes.user_path(conn, :index))
+  end
+end
