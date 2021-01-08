629e1fc05421607349b1d54c05478577bfbcd44a
CodigoDeSenior <migueljnm840@gmail.com>
2021-01-08 11:56:36 -0300

    Show todos on render function

--- Projects/live_view_todos/lib/live_view_todos_web/live/todo_live.ex
+++ Projects/live_view_todos/lib/live_view_todos_web/live/todo_live.ex
@@ -7,4 +7,12 @@ defmodule LiveViewTodosWeb.TodoLive do
     {:ok, assign(socket, todos: Todos.list_todos())}
   end
 
+  def render(assigns) do
+    ~L"""
+      <%= for todo <- @todos do %>
+        <div> <%= todo.title %> </div>
+      <% end %>
+    """
+  end
+
 end
